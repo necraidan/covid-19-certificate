ba53bfb8b7af2cf4fbad80585432451d2b95509a
Benjamin Auzanneau <benjamin.auzanneau.tm@gmail.com>
2020-03-18 12:06:16 +0100

    Update certificate.js

--- certificate.js
+++ certificate.js
@@ -153,7 +153,7 @@ $('#form-generate').addEventListener('submit', async event => {
   const profile = getProfile();
   const reason = getAndSaveReason()
   const pdfBlob = await generatePdf(profile, reason)
-  downloadBlob(pdfBlob, `attestation-${profile.name.replace('\'', '').split(' ').join('_')}.pdf`)
+  downloadBlob(pdfBlob, `attestation-${profile.name.replace('\'', '_').replace(' ', '_')}.pdf`)
 })
 
 restoreReason()
